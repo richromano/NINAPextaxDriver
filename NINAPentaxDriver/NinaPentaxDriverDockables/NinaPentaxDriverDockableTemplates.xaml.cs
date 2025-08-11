cb5fe411ef771b3f3b2963da570827971ca8353d
Richard Romano <richromano.rr@gmail.com>
2025-08-11 18:27:36 -0400

    Starting over

--- NINAPentaxDriver/NinaPentaxDriverDockables/NinaPentaxDriverDockableTemplates.xaml.cs
+++ NINAPentaxDriver/NinaPentaxDriverDockables/NinaPentaxDriverDockableTemplates.xaml.cs
@@ -5,22 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace Rtg.NINA.NinaPentaxDriver.NinaPentaxDriverDockables {
     [Export(typeof(ResourceDictionary))]
     public partial class MyPluginDockableTemplates : ResourceDictionary {
         public MyPluginDockableTemplates() {
             InitializeComponent();
-            //Fstops.Add({ 0,"Hello"});
         }
-
-        public class FSManager {
-            public long FSCode {  get; set; }
-            public string FSName { get; set; }
-        }
-
-        public List<FSManager> Fstops { get; set; }
     }
 }
